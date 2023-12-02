419b1ddc0d48f81e0a65343c150572378c02ba60
mykoza <48719773+mykoza@users.noreply.github.com>
2023-12-03 00:56:44 +0100

    Day 02 - Part 1

--- Program.cs
+++ Program.cs
@@ -3,5 +3,8 @@
 var day1 = new Day1();
 day1.Run();
 
+var day2 = new Day2();
+day2.Run();
+
 Console.WriteLine("Press any key to exit...");
 Console.ReadLine();
---
+++ Solutions/02/Day2.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode2023;
+
+public class Day2 : Solution
+{
+    protected override int DayNumber {get; init;} = 2;
+
+    protected override string LogicPart1()
+    {
+        var possibleGames = new int[_inputLines.Length];
+        var maxNumbers = new Dictionary<string, int> (){
+            {"red", 12 },
+            {"green", 13},
+            {"blue", 14}
+        };
+
+        for (int i = 0; i < _inputLines.Length; i++)
+        {
+            string line = _inputLines[i];
+            var split = line.Split(':');
+            var game = split[0];
+            var gameId = game.Split(' ')[1];
+            string[] sets = split[1].Split(';').Select(x => x.Trim()).ToArray();
+            var gamePossible = true;
+
+            foreach (var set in sets)
+            {
+                string[] subsets = set.Split(',').Select(x => x.Trim()).ToArray();
+
+                foreach (var subset in subsets)
+                {
+                    var splitSubset = subset.Split(' ');
+                    var number = int.Parse(splitSubset[0].ToString());
+                    var color = splitSubset[1];
+
+                    if (number > maxNumbers[color])
+                    {
+                        gamePossible = false;
+                        break;
+                    }
+                }
+                
+                if (!gamePossible)
+                {
+                    break;
+                }
+            }
+
+            if (gamePossible)
+            {
+                possibleGames[i] = int.Parse(gameId);
+            }
+        }
+
+        return possibleGames.Sum().ToString();
+    }
+
+    protected override string LoginPart2()
+    {
+        throw new NotImplementedException();
+    }
+}
