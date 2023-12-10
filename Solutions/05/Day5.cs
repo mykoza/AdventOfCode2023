fa208954f0402b9dfc779f45b0931da67a53f505
mykoza <48719773+mykoza@users.noreply.github.com>
2023-12-10 16:55:07 +0100

    Day 05 - Cleanup

--- Solutions/05/Day5.cs
+++ Solutions/05/Day5.cs
@@ -2,102 +2,32 @@
 
 public class Day5 : Solution
 {
+    private Map _seedToSoil = new();
+    private Map _soilToFertilizer = new();
+    private Map _fertilizerToWater = new();
+    private Map _waterToLight = new();
+    private Map _lightToTemperature = new();
+    private Map _temperatureToHumidity = new();
+    private Map _humidityToLocation = new();
+
     protected override int DayNumber { get; init; } = 5;
 
     protected override string LogicPart1()
     {
-        var seeds = new List<long>();
-        var seedToSoil = new Map();
-        var seedToSoilLines = new List<string>();
-        var soilToFertilizer = new Map();
-        var soilToFertilizerLines = new List<string>();
-        var fertilizerToWater = new Map();
-        var fertilizerToWaterLines = new List<string>();
-        var waterToLight = new Map();
-        var waterToLightLines = new List<string>();
-        var lightToTemperature = new Map();
-        var lightToTemperatureLines = new List<string>();
-        var temperatureToHumidity = new Map();
-        var temperatureToHumidityLines = new List<string>();
-        var humidityToLocation = new Map();
-        var humidityToLocationLines = new List<string>();
+        var seeds = _inputLines[0][7..].Split(' ').Select(long.Parse).ToList();
 
-        for (int i = 0; i < _inputLines.Length; i++)
-        {
-            string? line = _inputLines[i];
-
-            if (line.StartsWith("seeds:"))
-            {
-                seeds = line[7..].Split(' ').Select(long.Parse).ToList();
-            }
-            else if (line == "seed-to-soil map:")
-            {
-                while (_inputLines[++i] != "")
-                {
-                    seedToSoilLines.Add(_inputLines[i]);
-                }
-                seedToSoil = ParseMap(seedToSoilLines);
-            }
-            else if (line == "soil-to-fertilizer map:")
-            {
-                while (_inputLines[++i] != "")
-                {
-                    soilToFertilizerLines.Add(_inputLines[i]);
-                }
-                soilToFertilizer = ParseMap(soilToFertilizerLines);
-            }
-            else if (line == "fertilizer-to-water map:")
-            {
-                while (_inputLines[++i] != "")
-                {
-                    fertilizerToWaterLines.Add(_inputLines[i]);
-                }
-                fertilizerToWater = ParseMap(fertilizerToWaterLines);
-            }
-            else if (line == "water-to-light map:")
-            {
-                while (_inputLines[++i] != "")
-                {
-                    waterToLightLines.Add(_inputLines[i]);
-                }
-                waterToLight = ParseMap(waterToLightLines);
-            }
-            else if (line == "light-to-temperature map:")
-            {
-                while (_inputLines[++i] != "")
-                {
-                    lightToTemperatureLines.Add(_inputLines[i]);
-                }
-                lightToTemperature = ParseMap(lightToTemperatureLines);
-            }
-            else if (line == "temperature-to-humidity map:")
-            {
-                while (_inputLines[++i] != "")
-                {
-                    temperatureToHumidityLines.Add(_inputLines[i]);
-                }
-                temperatureToHumidity = ParseMap(temperatureToHumidityLines);
-            }
-            else if (line == "humidity-to-location map:")
-            {
-                while (i < _inputLines.Length - 1 && _inputLines[++i] != "")
-                {
-                    humidityToLocationLines.Add(_inputLines[i]);
-                }
-                humidityToLocation = ParseMap(humidityToLocationLines);
-            }
-        }
+        ParseInput();
 
         var locations = new List<long>();
         foreach (var seed in seeds)
         {
-            var soil = seedToSoil.GetDestination(seed);
-            var fertilizer = soilToFertilizer.GetDestination(soil);
-            var water = fertilizerToWater.GetDestination(fertilizer);
-            var light = waterToLight.GetDestination(water);
-            var temperature = lightToTemperature.GetDestination(light);
-            var humidity = temperatureToHumidity.GetDestination(temperature);
-            var location = humidityToLocation.GetDestination(humidity);
+            var soil = _seedToSoil.GetDestination(seed);
+            var fertilizer = _soilToFertilizer.GetDestination(soil);
+            var water = _fertilizerToWater.GetDestination(fertilizer);
+            var light = _waterToLight.GetDestination(water);
+            var temperature = _lightToTemperature.GetDestination(light);
+            var humidity = _temperatureToHumidity.GetDestination(temperature);
+            var location = _humidityToLocation.GetDestination(humidity);
 
             locations.Add(location);
         }
@@ -110,7 +40,80 @@ public class Day5 : Solution
         throw new NotImplementedException();
     }
 
-    protected static Map ParseMap(List<string> maps)
+    private void ParseInput()
+    {
+        var seedToSoilLines = new List<string>();
+        var soilToFertilizerLines = new List<string>();
+        var fertilizerToWaterLines = new List<string>();
+        var waterToLightLines = new List<string>();
+        var lightToTemperatureLines = new List<string>();
+        var temperatureToHumidityLines = new List<string>();
+        var humidityToLocationLines = new List<string>();
+
+        for (int i = 2; i < _inputLines.Length; i++)
+        {
+            string? line = _inputLines[i];
+
+            if (line == "seed-to-soil map:")
+            {
+                while (_inputLines[++i] != "")
+                {
+                    seedToSoilLines.Add(_inputLines[i]);
+                }
+                _seedToSoil = ParseMap(seedToSoilLines);
+            }
+            else if (line == "soil-to-fertilizer map:")
+            {
+                while (_inputLines[++i] != "")
+                {
+                    soilToFertilizerLines.Add(_inputLines[i]);
+                }
+                _soilToFertilizer = ParseMap(soilToFertilizerLines);
+            }
+            else if (line == "fertilizer-to-water map:")
+            {
+                while (_inputLines[++i] != "")
+                {
+                    fertilizerToWaterLines.Add(_inputLines[i]);
+                }
+                _fertilizerToWater = ParseMap(fertilizerToWaterLines);
+            }
+            else if (line == "water-to-light map:")
+            {
+                while (_inputLines[++i] != "")
+                {
+                    waterToLightLines.Add(_inputLines[i]);
+                }
+                _waterToLight = ParseMap(waterToLightLines);
+            }
+            else if (line == "light-to-temperature map:")
+            {
+                while (_inputLines[++i] != "")
+                {
+                    lightToTemperatureLines.Add(_inputLines[i]);
+                }
+                _lightToTemperature = ParseMap(lightToTemperatureLines);
+            }
+            else if (line == "temperature-to-humidity map:")
+            {
+                while (_inputLines[++i] != "")
+                {
+                    temperatureToHumidityLines.Add(_inputLines[i]);
+                }
+                _temperatureToHumidity = ParseMap(temperatureToHumidityLines);
+            }
+            else if (line == "humidity-to-location map:")
+            {
+                while (i < _inputLines.Length - 1 && _inputLines[++i] != "")
+                {
+                    humidityToLocationLines.Add(_inputLines[i]);
+                }
+                _humidityToLocation = ParseMap(humidityToLocationLines);
+            }
+        }
+    }
+
+    private static Map ParseMap(List<string> maps)
     {
         var map = new Map();
 
@@ -132,7 +135,7 @@ public class Map
 {
     private readonly List<Range> _ranges = [];
 
-    public Map() {}
+    public Map() { }
 
     public Map(List<Range> ranges)
     {
@@ -151,7 +154,7 @@ public class Map
         {
             if (range.TryGetDestination(source, out destination))
             {
-                break;
+                return destination;
             }
         }
 
