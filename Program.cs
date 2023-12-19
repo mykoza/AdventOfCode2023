3d4c0ecbde430bc0e702fb15bf654988bf627e48
mykoza <48719773+mykoza@users.noreply.github.com>
2023-12-19 14:30:54 +0100

    Day 11 - Part 1

--- Program.cs
+++ Program.cs
@@ -7,11 +7,12 @@ Solution[] days = [
     // new Day3(),
     // new Day4(),
     // new Day5(),
-    new Day6(),
-    new Day7(),
-    new Day8(),
-    new Day9(),
-    new Day10(),
+    // new Day6(),
+    // new Day7(),
+    // new Day8(),
+    // new Day9(),
+    // new Day10(),
+    new Day11(),
 ];
 
 foreach (var day in days)
---
+++ Solutions/11/Day11.cs
@@ -0,0 +1,83 @@
+﻿using AdventOfCode2023.Common;
+
+namespace AdventOfCode2023;
+
+public class Day11 : Solution
+{
+    private readonly List<string> _expandedImage = [];
+    protected override void BeforeLogic()
+    {
+        base.BeforeLogic();
+
+        // expand lines
+        for (int i = 0; i < inputLines.Length; i++)
+        {
+            string? line = inputLines[i];
+            _expandedImage.Add(line);
+            
+            if (!line.Contains('#'))
+            {
+                _expandedImage.Add(line);
+            }
+        }
+
+        // expand columns
+        for (int i = 0; i < _expandedImage[0].Length; i++)
+        {
+            var columnIsEmpty = !_expandedImage
+                .Select(x => x[i])
+                .Any(x => x == '#');
+            
+            if (columnIsEmpty)
+            {
+                for (int j = 0; j < _expandedImage.Count; j++)
+                {
+                    string? line = _expandedImage[j];
+                    _expandedImage[j] = line.Insert(i + 1, ".");
+                }
+
+                i++;
+            }
+        }
+    }
+
+    protected override string LogicPart1()
+    {
+        var galaxies = new List<(int X, int Y)>();
+
+        for (int i = 0; i < _expandedImage.Count; i++)
+        {
+            for (int j = 0; j < _expandedImage[i].Length; j++)
+            {
+                if (_expandedImage[i][j] == '#')
+                {
+                    galaxies.Add((j, i));
+                }
+            }
+        }
+
+        var length = 0;
+        for (int i = 0; i < galaxies.Count; i++)
+        {
+            (int X, int Y) galaxy = galaxies[i];
+
+            for (int j = i + 1; j < galaxies.Count; j++)
+            {
+                length += CalculateDistance(galaxies[i], galaxies[j]);
+            }
+        }
+
+        return length.ToString();
+    }
+
+    protected override string LogicPart2()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static int CalculateDistance((int X, int Y) galaxy, (int X, int Y) target)
+    {
+        return Math.Abs(galaxy.X - target.X) + Math.Abs(galaxy.Y - target.Y);
+    }
+}
+
