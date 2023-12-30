7e07de8046602740bf35de6a5b137466cf6bf9ee
mykoza <48719773+mykoza@users.noreply.github.com>
2023-12-30 20:28:23 +0100

    Day 14 - Part 2

--- Solutions/14/Day14.cs
+++ Solutions/14/Day14.cs
@@ -6,43 +6,162 @@ public class Day14 : Solution
 {
     protected override string LogicPart1()
     {
-        var tilted = TiltPlatform(inputLines);
+        var tilted = TiltPlatformNorth(inputLines);
         var load = CalculateTotalLoad(tilted);
         return load.ToString();
     }
 
     protected override string LogicPart2()
     {
-        throw new NotImplementedException();
+        var states = new Dictionary<int, long>();
+        var tilted = new List<string>(inputLines);
+
+        for (long i = 1; i <= 1_000_000_000; i++)
+        {
+            var hash = tilted.GetHashCode();
+
+            if (states.TryGetValue(hash, out long value))
+            {
+                var diff = i - value;
+                var count = (long)Math.Floor((decimal)(1_000_000_000 - i) / diff);
+                i += count * diff;
+                states = [];
+                continue;
+            }
+
+            states[hash] = i;
+
+            tilted = TiltPlatformNorth(tilted);
+            tilted = TiltPlatformWest(tilted);
+            tilted = TiltPlatformSouth(tilted);
+            tilted = TiltPlatformEast(tilted);
+        }
+
+        var load = CalculateTotalLoad(tilted);
+        return load.ToString();
     }
 
-    private static IList<string> TiltPlatform(IList<string> platform)
+    private static List<string> TiltPlatformNorth(IList<string> platform)
     {
-        for (int rowIdx = 1; rowIdx < platform.Count; rowIdx++)
+        var newPlatform = new List<string>(platform);
+        int rowLength = newPlatform[0].Length;
+        int rowCount = newPlatform.Count;
+
+        for (int rowIdx = 1; rowIdx < rowCount; rowIdx++)
         {
-            for (int columnIdx = 0; columnIdx < platform[0].Length; columnIdx++)
+            for (int columnIdx = 0; columnIdx < rowLength; columnIdx++)
             {
-                if (platform[rowIdx][columnIdx] == 'O')
+                if (newPlatform[rowIdx][columnIdx] == 'O')
                 {
                     var tempRowIdx = rowIdx;
-                    while (tempRowIdx > 0 && platform[tempRowIdx - 1][columnIdx] == '.')
+                    while (tempRowIdx > 0 && newPlatform[tempRowIdx - 1][columnIdx] == '.')
                     {
                         tempRowIdx--;
                     }
 
                     if (tempRowIdx != rowIdx)
                     {
-                        platform[rowIdx] = platform[rowIdx].Remove(columnIdx, 1).Insert(columnIdx, ".");
-                        platform[tempRowIdx] = platform[tempRowIdx].Remove(columnIdx, 1).Insert(columnIdx, "O");
+                        newPlatform[rowIdx] = newPlatform[rowIdx].Remove(columnIdx, 1).Insert(columnIdx, ".");
+                        newPlatform[tempRowIdx] = newPlatform[tempRowIdx].Remove(columnIdx, 1).Insert(columnIdx, "O");
                     }
                 }
             }
         }
 
-        return platform;
+        return newPlatform;
     }
 
-    private int CalculateTotalLoad(IList<string> platform)
+    private static List<string> TiltPlatformWest(IList<string> platform)
+    {
+        var newPlatform = new List<string>(platform);
+        int rowCount = newPlatform.Count;
+        int rowLength = newPlatform[0].Length;
+
+        for (int columnIdx = 0; columnIdx < rowLength; columnIdx++)
+        {
+            for (int rowIdx = 0; rowIdx < rowCount; rowIdx++)
+            {
+                if (newPlatform[rowIdx][columnIdx] == 'O')
+                {
+                    var tempColumnIdx = columnIdx;
+                    while (tempColumnIdx > 0 && newPlatform[rowIdx][tempColumnIdx - 1] == '.')
+                    {
+                        tempColumnIdx--;
+                    }
+
+                    if (tempColumnIdx != columnIdx)
+                    {
+                        newPlatform[rowIdx] = newPlatform[rowIdx].Remove(columnIdx, 1).Insert(columnIdx, ".");
+                        newPlatform[rowIdx] = newPlatform[rowIdx].Remove(tempColumnIdx, 1).Insert(tempColumnIdx, "O");
+                    }
+                }
+            }
+        }
+
+        return newPlatform;
+    }
+
+    private static List<string> TiltPlatformSouth(IList<string> platform)
+    {
+        var newPlatform = new List<string>(platform);
+        int rowLength = newPlatform[0].Length;
+        int rowCount = newPlatform.Count;
+
+        for (int rowIdx = rowCount - 1; rowIdx >= 0; rowIdx--)
+        {
+            for (int columnIdx = 0; columnIdx < rowLength; columnIdx++)
+            {
+                if (newPlatform[rowIdx][columnIdx] == 'O')
+                {
+                    var tempRowIdx = rowIdx;
+                    while (tempRowIdx < rowCount - 1 && newPlatform[tempRowIdx + 1][columnIdx] == '.')
+                    {
+                        tempRowIdx++;
+                    }
+
+                    if (tempRowIdx != rowIdx)
+                    {
+                        newPlatform[rowIdx] = newPlatform[rowIdx].Remove(columnIdx, 1).Insert(columnIdx, ".");
+                        newPlatform[tempRowIdx] = newPlatform[tempRowIdx].Remove(columnIdx, 1).Insert(columnIdx, "O");
+                    }
+                }
+            }
+        }
+
+        return newPlatform;
+    }
+
+    private static List<string> TiltPlatformEast(IList<string> platform)
+    {
+        var newPlatform = new List<string>(platform);
+        int rowCount = newPlatform.Count;
+        int rowLength = newPlatform[0].Length;
+
+        for (int columnIdx = rowLength - 1; columnIdx >= 0; columnIdx--)
+        {
+            for (int rowIdx = 0; rowIdx < rowCount; rowIdx++)
+            {
+                if (newPlatform[rowIdx][columnIdx] == 'O')
+                {
+                    var tempColumnIdx = columnIdx;
+                    while (tempColumnIdx < rowLength - 1 && newPlatform[rowIdx][tempColumnIdx + 1] == '.')
+                    {
+                        tempColumnIdx++;
+                    }
+
+                    if (tempColumnIdx != columnIdx)
+                    {
+                        newPlatform[rowIdx] = newPlatform[rowIdx].Remove(columnIdx, 1).Insert(columnIdx, ".");
+                        newPlatform[rowIdx] = newPlatform[rowIdx].Remove(tempColumnIdx, 1).Insert(tempColumnIdx, "O");
+                    }
+                }
+            }
+        }
+
+        return newPlatform;
+    }
+
+    private static int CalculateTotalLoad(IList<string> platform)
     {
         var rowCount = platform.Count;
         var res = 0;
