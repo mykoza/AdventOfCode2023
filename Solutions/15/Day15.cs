ac760fa27d63e92798148037da05d186cc90944c
mykoza <48719773+mykoza@users.noreply.github.com>
2023-12-31 16:05:58 +0100

    Day 15 - Part 2

--- Solutions/15/Day15.cs
+++ Solutions/15/Day15.cs
@@ -1,3 +1,4 @@
+using System.Collections.Specialized;
 using AdventOfCode2023.Common;
 
 namespace AdventOfCode2023;
@@ -23,7 +24,36 @@ class Day15 : Solution
 
     protected override string LogicPart2()
     {
-        throw new NotImplementedException();
+        var boxes = new List<Box>(256);
+
+        for (int i = 0; i < 256; i++)
+        {
+            boxes.Add(new Box());
+        }
+
+        foreach (var step in _initializationSeq)
+        {
+            if (step.Contains('='))
+            {
+                var split = step.Split('=');
+                var labelHash = ComputeHash(split[0]);
+                boxes[labelHash].AddLens(new Lens(split[0], int.Parse(split[1])));
+            }
+            else if (step.Contains('-'))
+            {
+                var split = step.Split('-');
+                var labelHash = ComputeHash(split[0]);
+                boxes[labelHash].RemoveLens(split[0]);
+            }
+        }
+
+        var res = 0;
+        for (int i = 0; i < boxes.Count; i++)
+        {
+            res += (i + 1) * boxes[i].FocusingPower();
+        }
+
+        return res.ToString();
     }
 
     private static int ComputeHash(string step)
@@ -40,3 +70,38 @@ class Day15 : Solution
         return currentVal;
     }
 }
+
+class Box
+{
+    private readonly OrderedDictionary _lenses = [];
+
+    public void AddLens(Lens lens)
+    {
+        if (_lenses.Contains(lens.Label))
+        {
+            _lenses[lens.Label] = lens.FocalLength;
+            return;
+        }
+
+        _lenses.Add(lens.Label, lens.FocalLength);
+    }
+
+    public void RemoveLens(string label)
+    {
+        _lenses.Remove(label);
+    }
+
+    public int FocusingPower()
+    {
+        var res = 0;
+
+        for (int i = 0; i < _lenses.Count; i++)
+        {
+            res += (i + 1) * (int)_lenses[i];
+        }
+
+        return res;
+    }
+}
+
+record Lens(string Label, int FocalLength);
